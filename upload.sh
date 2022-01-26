8ad2e96280f564f9794193224025321b2322074a
Azure-007 <64735082+Azure-007@users.noreply.github.com>
2022-01-26 18:57:43 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,63 +1,11 @@
 #!/bin/bash
 
-# A Function to Send Posts to Telegram
-telegram_message() {
-	curl -s -X POST "https://api.telegram.org/bot${TG_TOKEN}/sendMessage" -d chat_id="${TG_CHAT_ID}" \
-	-d text="$1"
+set -e
+set -x
+
+up(){
+	curl --upload-file $1 https://transfer.sh/$(basename $1); echo
+	# 14 days, 10 GB limit
 }
 
-# Change to the Source Directory
-cd $SYNC_PATH
-
-# Color
-ORANGE='\033[0;33m'
-
-# Display a message
-echo "============================"
-echo "Uploading the Build..."
-echo "============================"
-
-# Change to the Output Directory
-cd out/target/product/${DEVICE}
-
-# Set FILENAME var
-FILENAME=$(echo $OUTPUT)
-
-# Upload to oshi.at
-if [ -z "$TIMEOUT" ];then
-    TIMEOUT=20160
-fi
-
-# Upload to WeTransfer
-# NOTE: the current Docker Image, "registry.gitlab.com/sushrut1101/docker:latest", includes the 'transfer' binary by Default
-transfer wet $FILENAME > link.txt || { echo "ERROR: Failed to Upload the Build!" && exit 1; }
-
-# Mirror to oshi.at
-curl -T $FILENAME https://oshi.at/${FILENAME}/${OUTPUT} > mirror.txt || { echo "WARNING: Failed to Mirror the Build!"; }
-
-DL_LINK=$(cat link.txt | grep Download | cut -d\  -f3)
-MIRROR_LINK=$(cat mirror.txt | grep Download | cut -d\  -f1)
-
-# Show the Download Link
-echo "=============================================="
-echo "Download Link: ${DL_LINK}" || { echo "ERROR: Failed to Upload the Build!"; }
-echo "Mirror: ${MIRROR_LINK}" || { echo "WARNING: Failed to Mirror the Build!"; }
-echo "=============================================="
-
-# Send the Message on Telegram
-telegram_message \
-"
-🦊 OrangeFox Recovery CI
-
-✅ Build Completed Successfully!
-
-📱 Device: ${DEVICE}
-🌲 Device Tree: ${DT_LINK}
-🖥 Build System: ${FOX_BRANCH}
-⬇️ Download Link: ${DL_LINK}
-📅 Date: $(date +'%d %B %Y')
-⏱ Time: $(date +"%T")
-"
-
-# Exit
-exit 0
+up out/target/product/raphael/*.zip
