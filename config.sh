ef7c640de145ca4d569d9ebcdaa5ac86130f616b
moonitime <97696888+moonitime@users.noreply.github.com>
2022-03-12 12:27:13 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,9 +2,9 @@
 
 # Device
 export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export DT_LINK="https://gitlab.com/OrangeFox/device/lavender -b fox_9.0"
 
-export DEVICE="beryllium"
+export DEVICE="lavender"
 export OEM="xiaomi"
 export TARGET="recoveryimage"
 
