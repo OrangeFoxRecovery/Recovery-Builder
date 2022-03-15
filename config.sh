6f21651da4da1197422a67903030a91b41e914eb
Abhi2354 <100251020+Abhi2354@users.noreply.github.com>
2022-03-15 11:28:57 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://github.com/karan2354/android_recovery_xiaomi_olivewood.git -b olivewood-ofox"
+export FOX_BRANCH="fox_11.0"
+export DT_LINK="https://github.com/karan2354/android_recovery_xiaomi_olivewood.git -b fox_11.0"
 
 export DEVICE="olivewood"
 export OEM="xiaomi"
@@ -10,13 +10,8 @@ export TARGET="recoveryimage"
 
 export OUTPUT="OrangeFox*.zip"
 
-# Kernel Source
-# Uncomment the next line if you want to clone a kernel source.
-#export KERNEL_SOURCE="https://gitlab.com/OrangeFox/kernel/mojito.git"
-#export PLATFORM="sdm439" # Leave it commented if you want to clone the kernel to kernel/$OEM/$DEVICE
-
 # Extra Command
-export EXTRA_CMD="git clone https://github.com/OrangeFoxRecovery/Avatar.git misc"
+export EXTRA_CMD=""
 
 # Not Recommended to Change
 export SYNC_PATH="$HOME/work" # Full (absolute) path.
@@ -24,10 +19,3 @@ export USE_CCACHE=1
 export CCACHE_SIZE="50G"
 export CCACHE_DIR="$HOME/work/.ccache"
 export J_VAL=16
-
-if [ ! -z "$PLATFORM" ]; then
-    export KERNEL_PATH="kernel/$OEM/$PLATFORM"
-else
-    export KERNEL_PATH="kernel/$OEM/$DEVICE"
-fi
-export DT_PATH="device/$OEM/$DEVICE"
