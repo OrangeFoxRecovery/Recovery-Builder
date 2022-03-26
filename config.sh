41798a302b5605d02bb1b93dbeb6607fb2efaccb
Dmitry <42121541+dkpost3@users.noreply.github.com>
2022-03-26 14:46:33 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,18 +2,18 @@
 
 # Device
 export FOX_BRANCH="fox_11.0"
-export DT_LINK="https://github.com/OrangeFoxRecovery/device_oneplus_Nord.git -b fox_11.0"
+export DT_LINK="https://github.com/OrangeFoxRecovery/device_oneplus_avicii.git -b fox_10.0"
 
-export DEVICE="Nord"
+export DEVICE="avicii"
 export OEM="oneplus"
 export TARGET="recoveryimage"
 
-export OUTPUT="OrangeFox*.zip"
+export OUTPUT="OrangeFox.zip"
 
 # Kernel Source
 # Uncomment the next line if you want to clone a kernel source.
-export KERNEL_SOURCE="https://github.com/OnePlusOSS/android_kernel_oneplus_sm7250.git -b oneplus/SM7250_R_11.0"
-export PLATFORM="sm7250" # Leave it commented if you want to clone the kernel to kernel/$OEM/$DEVICE
+#export KERNEL_SOURCE="https://github.com/OnePlusOSS/android_kernel_oneplus_sm7250.git -b oneplus/SM7250_R_11.0"
+#export PLATFORM="sm7250" # Leave it commented if you want to clone the kernel to kernel/$OEM/$DEVICE
 
 # Extra Command
 export EXTRA_CMD="git clone https://github.com/OrangeFoxRecovery/Avatar.git misc"
