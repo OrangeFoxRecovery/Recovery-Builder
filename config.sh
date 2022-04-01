8c28667b1373bd52e23453f44466183a392e0ef7
CLHEX <100755123+clhexftw@users.noreply.github.com>
2022-04-01 19:18:02 +0530

    um does it matter

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_11.0"
+export DT_LINK="https://github.com/TeamWin/android_device_xiaomi_vayu.git -b android-11"
 
-export DEVICE="beryllium"
+export DEVICE="vayu"
 export OEM="xiaomi"
 export TARGET="recoveryimage"
 
