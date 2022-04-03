be00078e623690e08618502cb0640dfec928bab2
CLHEX <100755123+clhexftw@users.noreply.github.com>
2022-04-03 15:36:55 +0530

    fixed sizings

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_11.0"
+export DT_LINK="https://gitlab.com/OrangeFox/device/vayu.git -b fox_11.0"
 
-export DEVICE="beryllium"
+export DEVICE="vayu"
 export OEM="xiaomi"
 export TARGET="recoveryimage"
 
