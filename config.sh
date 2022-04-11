1f3f818e033367e3a489ab6f990e13239467f5d9
Johx22 <johannjose2004@gmail.com>
2022-04-11 12:44:46 +0300

    Build Samsung

--- config.sh
+++ config.sh
@@ -2,10 +2,10 @@
 
 # Device
 export FOX_BRANCH="fox_11.0"
-export DT_LINK="https://github.com/Johx22/device_oppo_OP4F2F -b fox_11.0"
+export DT_LINK="https://github.com/Johx22/android_device_samsung_j4lte -b fox_9.0"
 
-export DEVICE="OP4F2F"
-export OEM="oppo"
+export DEVICE="j4ltejx"
+export OEM="samsung"
 export TARGET="recoveryimage"
 
 export OUTPUT="OrangeFox*.zip"
