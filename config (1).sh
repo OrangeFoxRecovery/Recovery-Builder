21d9a4a9e71006c183d00287e619b001b9bbaade
Dmitry <42121541+dkpost3@users.noreply.github.com>
2022-06-07 00:28:26 +0300

    Add files via upload

---
+++ config (1).sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Device
+export FOX_BRANCH="fox_10.0"
+export DT_LINK="https://github.com/dkpost3/device_oneplus_avicii-1"
+
+export DEVICE="avicii"
+export OEM="oneplus"
+
+# Build Target
+## "recoveryimage" - for A-Only Devices without using Vendor Boot
+## "bootimage" - for A/B devices without recovery partition (and without vendor boot)
+## "vendorbootimage" - for devices Using vendor boot for the recovery ramdisk (Usually for devices shipped with Android 12 or higher)
+export TARGET="adbd recoveryimage bootimage"
+export OUTPUT="OrangeFox*.zip"
+
+# Kernel Source
+# Uncomment the next line if you want to clone a kernel source.
+export KERNEL_SOURCE="https://github.com/OnePlusOSS/android_kernel_oneplus_sm7250.git"
+export PLATFORM="sm7250" # Leave it commented if you want to clone the kernel to kernel/$OEM/$DEVICE
+
+# Extra Command
+export EXTRA_CMD="git clone https://github.com/OrangeFoxRecovery/Avatar.git misc"
+
+# Magisk
+## Use the Latest Release of Magisk for the OrangeFox addon
+OF_USE_LATEST_MAGISK=true
+
+# Not Recommended to Change
+export SYNC_PATH="$HOME/work" # Full (absolute) path.
+export USE_CCACHE=1
+export CCACHE_SIZE="50G"
+export CCACHE_DIR="$HOME/work/.ccache"
+export J_VAL=16
+
+if [ ! -z "$PLATFORM" ]; then
+    export KERNEL_PATH="kernel/$OEM/$PLATFORM"
+else
+    export KERNEL_PATH="kernel/$OEM/$DEVICE"
+fi
+export DT_PATH="device/$OEM/$DEVICE"
