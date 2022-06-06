1cb252e1a400a685dcb4018e929195fd13a2a4e7
Dmitry <42121541+dkpost3@users.noreply.github.com>
2022-06-06 21:42:43 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_11.0"
-export DT_LINK="https://github.com/OrangeFoxRecovery/device_oneplus_Nord.git -b fox_11.0"
+export FOX_BRANCH="fox_10.0"
+export DT_LINK="https://github.com/dkpost3/device_oneplus_avicii-1"
 
-export DEVICE="Nord"
+export DEVICE="avicii"
 export OEM="oneplus"
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
