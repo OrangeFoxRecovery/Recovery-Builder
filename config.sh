bd54eb96b746ea634edc1ea653175462fe6d02fa
Dmitry <42121541+dkpost3@users.noreply.github.com>
2022-06-12 13:16:34 +0300

    main
    Signed-off-by: Dmitry <42121541+dkpost3@users.noreply.github.com>

--- config.sh
+++ config.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_10.0"
-export DT_LINK="https://github.com/dkpost3/device_oneplus_avicii-1"
+export FOX_BRANCH="fox_11.0"
+export DT_LINK="https://github.com/dkpost3/device_oneplus_Nord"
 
-export DEVICE="avicii"
+export DEVICE="Nord"
 export OEM="oneplus"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
 ## "bootimage" - for A/B devices without recovery partition (and without vendor boot)
 ## "vendorbootimage" - for devices Using vendor boot for the recovery ramdisk (Usually for devices shipped with Android 12 or higher)
-export TARGET="recoveryimage"
+export TARGET="adbd recoveryimage"
 export OUTPUT="OrangeFox*.zip"
 
 # Kernel Source
 # Uncomment the next line if you want to clone a kernel source.
-export KERNEL_SOURCE="https://github.com/OnePlusOSS/android_kernel_oneplus_sm7250.git"
-export PLATFORM="sm7250" # Leave it commented if you want to clone the kernel to kernel/$OEM/$DEVICE
+export KERNEL_SOURCE="https://github.com/OnePlusOSS/android_kernel_oneplus_sm7250.git -b oneplus/SM7250_R_11.0"
+export PLATFORM="https://github.com/OnePlusOSS/android_vendor_qcom_opensource_audio_kernel_sm7250.git -b oneplus/SM7250_R_11.0" # Leave it commented if you want to clone the kernel to kernel/$OEM/$DEVICE
 
 # Extra Command
 export EXTRA_CMD="git clone https://github.com/OrangeFoxRecovery/Avatar.git misc"
