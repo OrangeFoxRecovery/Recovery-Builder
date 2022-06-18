e8a859038a2ef8a57064d1855b7e96569cc0c802
Bhav Gupta <83651732+Bhav06@users.noreply.github.com>
2022-06-18 23:35:52 +0530

    vince: Ofox Build

--- config.sh
+++ config.sh
@@ -2,9 +2,9 @@
 
 # Device
 export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export DT_LINK="https://github.com/Bhav06/device_xiaomi_vince_ofox.git -b fox_9.0"
 
-export DEVICE="beryllium"
+export DEVICE="vince"
 export OEM="xiaomi"
 
 # Build Target
