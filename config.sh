f0b8ca2982b933e5e91a0078408c52bef0a0f647
Johann <88674706+Johx22@users.noreply.github.com>
2022-06-27 17:37:23 +0300

    Build OPPO A54

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_11.0"
+export DT_LINK="https://github.com/Johx22/device_oppo_OP4F2F -b test"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="OP4F2F"
+export OEM="oppo"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
