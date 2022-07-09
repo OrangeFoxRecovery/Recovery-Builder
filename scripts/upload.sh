36ad3cfab64b05bdd50cb3d19466a13458636f5c
pedrozzz0 <guitopzika26@gmail.com>
2022-07-09 12:26:23 -0300

    Make scripts code more consistent
    ** Prevent word globbing and remove unnecessary ifs
    
    Signed-off-by: pedrozzz0 <guitopzika26@gmail.com>

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Source Vars
-source $CONFIG
+source "$CONFIG"
 
 # A Function to Send Posts to Telegram
 telegram_message() {
 	curl -s -X POST "https://api.telegram.org/bot${TG_TOKEN}/sendMessage" \
-	-d chat_id="${TG_CHAT_ID}" \
+	-d chat_id="$TG_CHAT_ID" \
 	-d parse_mode="HTML" \
 	-d text="$1"
 }
 
 # Change to the Source Directory
-cd $SYNC_PATH
+cd "$SYNC_PATH"
 
 # Color
 ORANGE='\033[0;33m'
@@ -23,54 +23,51 @@ echo "Uploading the Build..."
 echo "============================"
 
 # Change to the Output Directory
-cd out/target/product/${DEVICE}
+cd out/target/product/"$DEVICE"
 
 # Set FILENAME var
-FILENAME=$(echo $OUTPUT)
+FILENAME=$(echo "$OUTPUT")
 
 # Upload to oshi.at
-if [ -z "$TIMEOUT" ];then
-    TIMEOUT=20160
-fi
+[[ -z "$TIMEOUT" ]] && TIMEOUT=20160
 
 # Upload to WeTransfer
 # NOTE: the current Docker Image, "registry.gitlab.com/sushrut1101/docker:latest", includes the 'transfer' binary by Default
-transfer wet $FILENAME > link.txt || { echo "ERROR: Failed to Upload the Build!" && exit 1; }
+transfer wet "$FILENAME" > link.txt || { echo "ERROR: Failed to Upload the Build!" && exit 1; }
 
 # Mirror to oshi.at
-curl -T $FILENAME https://oshi.at/${FILENAME}/${OUTPUT} > mirror.txt || { echo "WARNING: Failed to Mirror the Build!"; }
+curl -T "$FILENAME" https://oshi.at/"$FILENAME"/"$OUTPUT" > mirror.txt || { echo "WARNING: Failed to Mirror the Build!"; }
 
 DL_LINK=$(cat link.txt | grep Download | cut -d\  -f3)
 MIRROR_LINK=$(cat mirror.txt | grep Download | cut -d\  -f1)
 
 # Show the Download Link
 echo "=============================================="
-echo "Download Link: ${DL_LINK}" || { echo "ERROR: Failed to Upload the Build!"; }
-echo "Mirror: ${MIRROR_LINK}" || { echo "WARNING: Failed to Mirror the Build!"; }
+echo "Download Link: $DL_LINK" || { echo "ERROR: Failed to Upload the Build!"; }
+echo "Mirror: $MIRROR_LINK" || { echo "WARNING: Failed to Mirror the Build!"; }
 echo "=============================================="
 
 DATE_L=$(date +%d\ %B\ %Y)
 DATE_S=$(date +"%T")
 
 # Send the Message on Telegram
-echo -e \
-"
+echo "
 🦊 OrangeFox Recovery CI
 
 ✅ Build Completed Successfully!
 
-📱 Device: "${DEVICE}"
-🖥 Build System: "${FOX_BRANCH}"
-⬇️ Download Link: <a href=\"${DL_LINK}\">Here</a>
-📅 Date: "$(date +%d\ %B\ %Y)"
-⏱ Time: "$(date +%T)"
+📱 Device: $DEVICE
+🖥 Build System: $FOX_BRANCH
+⬇️ Download Link: <a href=\$DL_LINK\>Here</a>
+📅 Date: $(date +%d\ %B\ %Y)
+⏱ Time: $(date +%T)
 " > tg.html
 
 TG_TEXT=$(< tg.html)
 
 telegram_message "$TG_TEXT"
 
-echo " "
+echo ""
 
 # Exit
 exit 0
