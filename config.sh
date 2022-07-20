e72b0dc9b0fd5511b845a0a0d516287980ef4e63
Fr0ztyy43 <82140208+Fr0ztyy43@users.noreply.github.com>
2022-07-20 12:23:20 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export FOX_BRANCH="fox_11.0"
-export DT_LINK="https://github.com/Fr0ztyy43/twrp_redmi_begonia -b 11.0"
+export DT_LINK="https://github.com/Fr0ztyy43/twrp_xiaomi_begonia -b 11.0"
 
 export DEVICE="begonia"
 export OEM="xiaomi"
