b3fd52d9fcaa4f023ca8dbbb13584767662fbf73
blacklytning <blacklytning@yandex.com>
2022-08-24 18:45:02 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -22,7 +22,7 @@ DEPS=(
 )
 
 # Extra Command
-#export EXTRA_CMD="export OF_MAINTAINER=SusurootNoobda"
+#export EXTRA_CMD="export OF_MAINTAINER=SusuroootNoobda"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
