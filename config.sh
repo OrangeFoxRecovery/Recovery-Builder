30d8adf88c943314783293120277c03dec033ff8
Ravinderrohilla <88099034+Ravinderrohilla@users.noreply.github.com>
2022-10-15 18:08:53 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox r11.1"
+export DT_LINK="https://github.com/Powerfull11/android_device_realme_RMX2117-android-11.git"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="RMX2117"
+export OEM="Realme"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
