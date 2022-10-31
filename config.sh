1aee603362e791b4b51d75b5379eec21ec6e2026
Diwas1111 <117113199+Diwas1111@users.noreply.github.com>
2022-11-01 00:09:09 +0545

    Adjustment for building with on5xelte tree

--- config.sh
+++ config.sh
@@ -2,10 +2,10 @@
 
 # Device
 export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export DT_LINK="https://github.com/Diwas007/android_device_samsung_on5xelte.git -b fox_9.0"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="on5xelte"
+export OEM="samsung"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
