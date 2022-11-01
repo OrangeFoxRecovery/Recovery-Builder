69fa2860a28bbada4b959cb9710d66d61076df16
edenefri <115766719+edenefri@users.noreply.github.com>
2022-11-01 21:23:13 +0700

    Update config.sh
    chanve

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export FOX_BRANCH="fox_11.0"
-export DT_LINK="https://github.com/edenefri/recovery-device-redmi-viva -b android-11"
+export DT_LINK="https://github.com/edenefri/recovery-device-redmi-viva -b fox_11.0"
 
 export DEVICE="viva"
 export OEM="xiaomi"
