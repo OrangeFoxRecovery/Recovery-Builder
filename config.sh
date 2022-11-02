f126db69239a75f9bedaac7a4feb4d3c982adc5f
Hasajapathirana <80383870+Hasajapathirana@users.noreply.github.com>
2022-11-02 06:24:51 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_12.1"
+export DT_LINK="https://github.com/mohammad92/android_device_samsung_p3s -b android-12.1"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="p3s"
+export OEM="samsung"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
@@ -18,7 +18,7 @@ export OUTPUT="OrangeFox*.zip"
 # Additional Dependencies (eg: Kernel Source)
 # Format: "repo dest"
 DEPS=(
-    "https://github.com/OrangeFoxRecovery/Avatar.git misc"
+    "https://github.com/mohammad92/android_kernel_samsung_exynos2100.git misc"
 )
 
 # Extra Command
