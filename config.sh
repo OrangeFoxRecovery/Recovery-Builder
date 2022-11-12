daf7dc6358a9892bc37945c9ad25155a7b02e2cb
Sergeant Kakashi <111608769+sergeantkakashi@users.noreply.github.com>
2022-11-12 15:55:19 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://github.com/Velosh/android_device_samsung_a50dd -b fox-9.0"
+export DT_LINK="https://github.com/Velosh/android_device_samsung_a50dd.git -b fox-9.0"
 
 export DEVICE="a50dd"
 export OEM="samsung"
