a3c26d2c5af79c6fd955b3993e1318863994efb0
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2022-12-17 03:14:55 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_12.1"
+export DT_LINK="https://github.com/redznn/android_device_samsung_m23xq/ -b M23"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="m23xq"
+export OEM="samsung"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
@@ -19,10 +19,11 @@ export OUTPUT="OrangeFox*.zip"
 # Format: "repo dest"
 DEPS=(
     "https://github.com/OrangeFoxRecovery/Avatar.git misc"
+    "https://github.com/ZorEl212/android_kernel_samsung_m23xqs kernel/Samsung/m23xq"
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
+export EXTRA_CMD="export OF_MAINTAINER=mrsiri"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
