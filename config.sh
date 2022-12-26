60a8e727e08e9af6acb00f52f3b5093e7e5bac58
CyclicPoet44782 <75398436+CyclicPoet44782@users.noreply.github.com>
2022-12-25 22:24:29 -0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,9 +2,9 @@
 
 # Device
 export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git fox_13.0"
 
-export DEVICE="beryllium"
+export DEVICE="lancelot"
 export OEM="xiaomi"
 
 # Build Target
@@ -22,7 +22,7 @@ DEPS=(
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
+export EXTRA_CMD="export OF_MAINTAINER=CyclicPoet44782"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
