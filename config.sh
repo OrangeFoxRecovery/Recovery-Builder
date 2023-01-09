7cc93251e9ec84119543a6f67f378fe829b3052b
RizalEf <120234762+RizalEf75@users.noreply.github.com>
2023-01-09 18:59:17 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export FOX_BRANCH="fox_12.1"
-export DT_LINK="https://github.com/RizalEf75/device_xiaomi_viva-TWRP.git -b twrp-12.1"
+export DT_LINK="https://github.com/RizalEf75/test -b main"
 
 export DEVICE="viva"
 export OEM="xiaomi"
