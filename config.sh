002ed9609809afd7b67c283349bbdf76af761b51
rizalef75 <122612484+rizalef75@users.noreply.github.com>
2023-01-22 20:23:57 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export FOX_BRANCH="fox_12.1"
-export DT_LINK="https://github.com/rizalef75/viva_test -b twelve"
+export DT_LINK="https://github.com/rizalef75/viva_test -b p1"
 
 export DEVICE="viva"
 export OEM="xiaomi"
