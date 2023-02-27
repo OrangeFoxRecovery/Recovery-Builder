0117c3e00c9cccdb2163b9bb7014fccb4cca1660
rizalef77 <124004937+rizalef77@users.noreply.github.com>
2023-02-27 10:27:59 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -22,7 +22,7 @@ DEPS=(
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
+export EXTRA_CMD="export OF_MAINTAINER=rizalef77"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
@@ -34,4 +34,3 @@ export USE_CCACHE=1
 export CCACHE_SIZE="50G"
 export CCACHE_DIR="$HOME/work/.ccache"
 export J_VAL=16
-
