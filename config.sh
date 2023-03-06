35f2b58d719ecbb75962e8f870bc7447a88eeb56
d4fun <126586537+d4fun@users.noreply.github.com>
2023-03-06 12:56:45 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export FOX_BRANCH="fox_12.1"
-export DT_LINK="https://github.com/d4fun/twrp.git -b twelve"
+export DT_LINK="https://github.com/d4fun/twrp.git -b A12"
 
 export DEVICE="viva"
 export OEM="xiaomi"
