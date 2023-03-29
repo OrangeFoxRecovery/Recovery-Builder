8d6fb664a63d105a6ca59d22149f36ec38c19256
d4fun <126586537+d4fun@users.noreply.github.com>
2023-03-29 12:42:27 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export FOX_BRANCH="fox_12.1"
-export DT_LINK="https://github.com/d4fun/tviva.git -b 12"
+export DT_LINK="https://github.com/d4fun/tviva.git -b twelve"
 
 export DEVICE="viva"
 export OEM="xiaomi"
