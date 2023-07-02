252cfde6b141e0c7aa315f6b73b4cc02db1d8440
setupzx1 <137245203+setupzx1@users.noreply.github.com>
2023-07-02 22:06:50 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export FOX_BRANCH="fox_12.1"
-export DT_LINK="https://github.com/setupzx1/device_xiaomi_viva-recovery.git -b twelve"
+export DT_LINK="https://github.com/setupzx1/device_xiaomi_viva-recovery -b twelve"
 
 export DEVICE="viva"
 export OEM="xiaomi"
