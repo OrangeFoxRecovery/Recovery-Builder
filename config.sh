3446d85ecf1c9fc0e41883a5dce4e78993442d8c
marat2509 <93652988+marat2509@users.noreply.github.com>
2023-12-27 02:48:39 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_12.1"
+export DT_LINK="https://github.com/marat2509/twrp_xiaomi_sweet_k6a -b fox_12.1"
 
-export DEVICE="beryllium"
+export DEVICE="sweet_k6a"
 export OEM="xiaomi"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
 ## "bootimage" - for A/B devices without recovery partition (and without vendor boot)
 ## "vendorbootimage" - for devices Using vendor boot for the recovery ramdisk (Usually for devices shipped with Android 12 or higher)
-export TARGET="recoveryimage"
+export TARGET="adbd recoveryimage"
 
 export OUTPUT="OrangeFox*.zip"
 
 # Additional Dependencies (eg: Kernel Source)
 # Format: "repo dest"
 DEPS=(
-    "https://github.com/OrangeFoxRecovery/Avatar.git misc"
+    # "https://github.com/OrangeFoxRecovery/Avatar.git misc"
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
+# export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
