dd2ea76efb8ba638779d674cbf12280748a90d53
Cuma.Rull <102359107+cumaRull@users.noreply.github.com>
2024-01-13 00:43:45 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_12.1"
+export DT_LINK="https://github.com/cumaRull/twrp_realme_RMX3191.git -b master"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="even"
+export OEM="realme"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
@@ -22,7 +22,7 @@ DEPS=(
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
+export EXTRA_CMD="export OF_MAINTAINER=ismasrull"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
