d6922bc9e027fd54b0562ed74be2cb3f2074b584
nurfinzmuhamad14 <51253393+nurfinzmuhamad14@users.noreply.github.com>
2024-06-28 15:15:12 +0700

    Update config.sh
    add RMX2195

--- config.sh
+++ config.sh
@@ -2,10 +2,10 @@
 
 # Device
 export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export DT_LINK="https://github.com/nurfinzmuhamad14/TWRP_RMX2195.git -b android-10.0"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="RMX2195"
+export OEM="realme"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
@@ -22,7 +22,7 @@ DEPS=(
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
+export EXTRA_CMD="export OF_MAINTAINER=UdyneOS"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
